99cfa8527cbffadd3b97c9db0d66cf6c3cddf75f
hallsamir14 <hallsamir14@gmail.com>
2025-06-15 10:57:42 -0400

    update method references for client object

--- client/client_main.cpp
+++ client/client_main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[]) {
   FLAGS_logtostderr = 1; // Log all messages to stderr
   google::InitGoogleLogging("server_main");
   Client_Socket client = Client_Socket();
-  client.connectToServer();
-  client.sendMessage();
+  client.ConnectToServer();
+  client.SendMessage();
   return 0;
 }
