cff2a57911d9304019718f6d6c8f6f7307a9e733
hallsamir14 <hallsamir14@gmail.com>
2025-07-27 19:46:48 -0400

    Im0plement test functions for Start and Stop server socket methods

--- server/tests/server_test.cpp
+++ server/tests/server_test.cpp
@@ -1,14 +1,21 @@
 #include "server_socket.h"
 #include <gtest/gtest.h>
+#include <iostream>
 
 TEST(ServerSocketTest, TestServerSocketStart) {
   // TODO complete impolementation
-  Server_Socket server;
-  EXPECT_TRUE(1);
+  auto mock_accept = [](int, struct sockaddr*, socklen_t*) { return 42; };
+  Server_Socket server(mock_accept);
+  EXPECT_NO_THROW(server.Start());
+  EXPECT_EQ(server.Get_status(),1);
 }
 
 TEST(ServerSocketTest, TestServerSocketStop) {
   // TODO complete impolementation
-  Server_Socket server;
-  EXPECT_FALSE(0);
+  auto mock_accept = [](int, struct sockaddr*, socklen_t*) { return 42; };
+  Server_Socket server(mock_accept);
+  server.Start();
+  EXPECT_EQ(server.Get_status(),1);
+  EXPECT_NO_THROW(server.Stop());
+  EXPECT_EQ(server.Get_status(),0);
 }
